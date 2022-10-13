fd561c97a4d5cd21f55f92e1f2ffe1dfca62a28a
jpwaldron2000 <jpwaldron2000@gmail.com>
2022-10-12 20:47:45 -0700

    red ghost neighbor detection

--- ghost.py
+++ ghost.py
@@ -1,19 +1,38 @@
 import pygame as pg
+from nodes import Node,NodeGroup
 
 class Ghost():
     def __init__(self, game,type, pac_pos):
         self.game = game
-        self.pac_pos = pac_pos
+        self.pacx= pac_pos[0]
+        self.pacy = pac_pos[1]
         self.screen = game.screen
-        #self.image = pg.image.load('images/redGhost.png')
-        #self.rect = self.image.get_rect()
+        self.image = pg.image.load('images/ghost.png')
+        self.rect = self.image.get_rect()
         self.type = type
+        self.x = self.rect.x
+        self.y = self.rect.y
 
-    def red(self): pass
+    def red(self): 
+        if self.atNode():
+            currentNode = self.NodeGroup.getNodeFromTiles(self.x,self.y)
+            up = currentNode.neighbors["UP"]
+            down = currentNode.neighbors["DOWN"]
+            right = currentNode.neighbors["RIGHT"]
+            left = currentNode.neighbors["LEFT"]
 
+
+
+            
     def pink(self): pass
 
     def orange(self):pass
 
     def blue(self): pass
+
+    def move(self, dir):pass
+
+    def atNode(self):
+        pass
+    
     
